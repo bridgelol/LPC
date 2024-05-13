8195a05b31b2f17b36e76f4a5093b5a695f41aae
bridge <haha@haha.com>
2024-05-14 00:36:18 +0200

    ugly but inv in chat lol

--- src/main/java/me/wikmor/lpc/LPC.java
+++ src/main/java/me/wikmor/lpc/LPC.java
@@ -1,13 +1,13 @@
 package me.wikmor.lpc;
 
-import io.papermc.paper.event.player.AsyncChatDecorateEvent;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import io.papermc.paper.event.player.AsyncChatEvent;
 import me.clip.placeholderapi.PlaceholderAPI;
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.ComponentLike;
-import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.TextReplacementConfig;
-import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer;
+import net.kyori.adventure.text.event.ClickEvent;
+import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import net.luckperms.api.LuckPerms;
 import net.luckperms.api.cacheddata.CachedMetaData;
@@ -20,52 +20,83 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.function.Function;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public final class LPC extends JavaPlugin implements Listener {
 
-	private static final Pattern HEX_PATTERN = Pattern.compile("&#([A-Fa-f0-9]{6})");
+    private static final Pattern HEX_PATTERN = Pattern.compile("&#([A-Fa-f0-9]{6})");
+    private final Cache<String, CachedInventory> cachedInventories = CacheBuilder.newBuilder().expireAfterWrite(30, TimeUnit.SECONDS).build();
+    private LuckPerms luckPerms;
 
-	private LuckPerms luckPerms;
-	
-	@Override
-	public void onEnable() {
-		// Load an instance of 'LuckPerms' using the services manager.
-		this.luckPerms = getServer().getServicesManager().load(LuckPerms.class);
+    @Override
+    public void onEnable() {
+        // Load an instance of 'LuckPerms' using the services manager.
+        this.luckPerms = getServer().getServicesManager().load(LuckPerms.class);
 
-		saveDefaultConfig();
-		getServer().getPluginManager().registerEvents(this, this);
-	}
+        saveDefaultConfig();
+        getServer().getPluginManager().registerEvents(this, this);
+    }
 
-	@Override
-	public boolean onCommand(final CommandSender sender, final Command command, final String label, final String[] args) {
-		if (args.length == 1 && "reload".equals(args[0])) {
-			reloadConfig();
+    @Override
+    public void onDisable() {
+        for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
+            if (onlinePlayer.getOpenInventory().getTopInventory().getHolder() instanceof InventorySnapshot) {
+                onlinePlayer.closeInventory();
+            }
+        }
+    }
 
-			sender.sendMessage(colorize("&aLPC has been reloaded."));
-			return true;
-		}
+    @Override
+    public boolean onCommand(final CommandSender sender, final Command command, final String label, final String[] args) {
+        if (label.equalsIgnoreCase("inv")) {
+            if (!(sender instanceof Player)) {
+                sender.sendMessage(colorize("&cYou must be a player to execute this command."));
+                return true;
+            }
 
-		return false;
-	}
+            if (args.length != 1) {
+                sender.sendMessage(colorize("&cUsage: /inv <player>"));
+                return true;
+            }
 
-	@Override
-	public List<String> onTabComplete(final CommandSender sender, final Command command, final String alias, final String[] args) {
-		if (args.length == 1)
-			return Collections.singletonList("reload");
+            CachedInventory cachedInventory = cachedInventories.getIfPresent(args[0].toLowerCase());
 
-		return new ArrayList<>();
-	}
+            if (cachedInventory == null) {
+                sender.sendMessage(colorize("&cInvalid inventory. User hasn't advertised their inventory in the last 30 seconds."));
+                return true;
+            }
+
+            Inventory inventory = new InventorySnapshot().getInventory();
+
+            inventory.setContents(cachedInventory.getContents());
+
+            ((Player) sender).openInventory(inventory);
+            return true;
+        }
+
+        if (args.length == 1 && "reload".equals(args[0])) {
+            reloadConfig();
+
+            sender.sendMessage(colorize("&aLPC has been reloaded."));
+            return true;
+        }
+
+        return false;
+    }
 
 	/*@EventHandler(priority = EventPriority.HIGH)
 	public void onChat(final AsyncPlayerChatEvent event) {
@@ -95,84 +126,118 @@ public final class LPC extends JavaPlugin implements Listener {
 				? translateHexColorCodes(message) : message.replace("%", "%%"));
 	}*/
 
-	@EventHandler(priority = EventPriority.HIGHEST)
-	public void onChat(final AsyncChatEvent event) {
-		Component component = event.message();
-		event.setCancelled(true);
+    @Override
+    public List<String> onTabComplete(final CommandSender sender, final Command command, final String alias, final String[] args) {
+        if (alias.equalsIgnoreCase("inv") || !sender.hasPermission("lpc.admin")) {
+            return Collections.emptyList();
+        }
 
-		if (event.getPlayer().hasPermission("lpc.chat.item")) {
-			component = component.replaceText(
-					TextReplacementConfig.builder()
-							.match(Pattern.compile("\\[item]|\\[i]"))
-							.replacement(builder -> formatItemInHand(event.getPlayer()))
-							.once()
-							.build()
-			);
-		}
+        if (args.length == 1) return Collections.singletonList("reload");
 
-		final Player player = event.getPlayer();
-		final CachedMetaData metaData = this.luckPerms.getPlayerAdapter(Player.class).getMetaData(player);
-		final String group = metaData.getPrimaryGroup();
+        return new ArrayList<>();
+    }
 
-		String format = getConfig().getString(getConfig().getString("group-formats." + group) != null ? "group-formats." + group : "chat-format")
-				.replace("{prefix}", metaData.getPrefix() != null ? metaData.getPrefix() : "")
-				.replace("{suffix}", metaData.getSuffix() != null ? metaData.getSuffix() : "")
-				.replace("{prefixes}", metaData.getPrefixes().keySet().stream().map(key -> metaData.getPrefixes().get(key)).collect(Collectors.joining()))
-				.replace("{suffixes}", metaData.getSuffixes().keySet().stream().map(key -> metaData.getSuffixes().get(key)).collect(Collectors.joining()))
-				.replace("{world}", player.getWorld().getName())
-				.replace("{name}", player.getName())
-				.replace("{displayname}", player.getDisplayName())
-				.replace("{username-color}", metaData.getMetaValue("username-color") != null ? metaData.getMetaValue("username-color") : "")
-				.replace("{message-color}", metaData.getMetaValue("message-color") != null ? metaData.getMetaValue("message-color") : "");
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onClick(InventoryClickEvent event) {
+        if (event.getInventory().getHolder() instanceof InventorySnapshot) {
+            event.setCancelled(true);
+        }
+    }
 
-		format = colorize(translateHexColorCodes(getServer().getPluginManager().isPluginEnabled("PlaceholderAPI") ? PlaceholderAPI.setPlaceholders(player, format) : format));
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onChat(final AsyncChatEvent event) {
+        Component component = event.message();
+        event.setCancelled(true);
 
-		final Component formatComponent = LegacyComponentSerializer.legacySection().deserialize(format);
-		Component finalComponent = component;
-		final Component toSend = formatComponent.replaceText(
-				TextReplacementConfig.builder()
-						.match(Pattern.compile("\\{message}|\\{msg}"))
-						.replacement(() -> finalComponent)
-						.once()
-						.build()
-		);
+        if (event.getPlayer().hasPermission("lpc.chat.item")) {
+            component = component.replaceText(TextReplacementConfig.builder().match(Pattern.compile("\\[item]|\\[i]")).replacement(builder -> formatItemInHand(event.getPlayer())).once().build());
+        }
 
-		for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
-			onlinePlayer.sendMessage(player, toSend);
-		}
-	}
+        if (event.getPlayer().hasPermission("lpc.chat.inventory")) {
+            component = component.replaceText(TextReplacementConfig.builder().match(Pattern.compile("\\[inventory]|\\[inv]")).replacement(builder -> formatInventory(event.getPlayer())).once().build());
+        }
 
-	private String colorize(final String message) {
-		return ChatColor.translateAlternateColorCodes('&', message);
-	}
+        final Player player = event.getPlayer();
+        final CachedMetaData metaData = this.luckPerms.getPlayerAdapter(Player.class).getMetaData(player);
+        final String group = metaData.getPrimaryGroup();
 
-	private String translateHexColorCodes(final String message) {
-		final char colorChar = ChatColor.COLOR_CHAR;
+        String format = getConfig().getString(getConfig().getString("group-formats." + group) != null ? "group-formats." + group : "chat-format").replace("{prefix}", metaData.getPrefix() != null ? metaData.getPrefix() : "").replace("{suffix}", metaData.getSuffix() != null ? metaData.getSuffix() : "").replace("{prefixes}", metaData.getPrefixes().keySet().stream().map(key -> metaData.getPrefixes().get(key)).collect(Collectors.joining())).replace("{suffixes}", metaData.getSuffixes().keySet().stream().map(key -> metaData.getSuffixes().get(key)).collect(Collectors.joining())).replace("{world}", player.getWorld().getName()).replace("{name}", player.getName()).replace("{displayname}", player.getDisplayName()).replace("{username-color}", metaData.getMetaValue("username-color") != null ? metaData.getMetaValue("username-color") : "").replace("{message-color}", metaData.getMetaValue("message-color") != null ? metaData.getMetaValue("message-color") : "");
 
-		final Matcher matcher = HEX_PATTERN.matcher(message);
-		final StringBuffer buffer = new StringBuffer(message.length() + 4 * 8);
+        format = colorize(translateHexColorCodes(getServer().getPluginManager().isPluginEnabled("PlaceholderAPI") ? PlaceholderAPI.setPlaceholders(player, format) : format));
 
-		while (matcher.find()) {
-			final String group = matcher.group(1);
+        final Component formatComponent = LegacyComponentSerializer.legacySection().deserialize(format);
+        Component finalComponent = component;
+        final Component toSend = formatComponent.replaceText(TextReplacementConfig.builder().match(Pattern.compile("\\{message}|\\{msg}")).replacement(() -> finalComponent).once().build());
 
-			matcher.appendReplacement(buffer, colorChar + "x"
-					+ colorChar + group.charAt(0) + colorChar + group.charAt(1)
-					+ colorChar + group.charAt(2) + colorChar + group.charAt(3)
-					+ colorChar + group.charAt(4) + colorChar + group.charAt(5));
-		}
+        for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
+            onlinePlayer.sendMessage(player, toSend);
+        }
+    }
 
-		return matcher.appendTail(buffer).toString();
-	}
+    private String colorize(final String message) {
+        return ChatColor.translateAlternateColorCodes('&', message);
+    }
 
-	private Component formatItemInHand(Player player) {
-		ItemStack itemInMainHand = player.getEquipment().getItemInMainHand();
+    private String translateHexColorCodes(final String message) {
+        final char colorChar = ChatColor.COLOR_CHAR;
 
-		if (!itemInMainHand.hasItemMeta() || !itemInMainHand.getItemMeta().hasDisplayName()) {
-			return Component.text("[" + WordUtils.capitalizeFully(itemInMainHand.getType().name().toLowerCase().replace("_", " ")) + (itemInMainHand.getAmount() > 1 ? " x" + itemInMainHand.getAmount() : "") + "]")
-					.hoverEvent(itemInMainHand.asHoverEvent());
-		}
+        final Matcher matcher = HEX_PATTERN.matcher(message);
+        final StringBuffer buffer = new StringBuffer(message.length() + 4 * 8);
 
-		return Component.text("[").append(itemInMainHand.getItemMeta().displayName()).append(Component.text((itemInMainHand.getAmount() > 1 ? (" x" + itemInMainHand.getAmount()) : "") + "]"))
-				.hoverEvent(itemInMainHand.asHoverEvent());
-	}
+        while (matcher.find()) {
+            final String group = matcher.group(1);
+
+            matcher.appendReplacement(buffer, colorChar + "x" + colorChar + group.charAt(0) + colorChar + group.charAt(1) + colorChar + group.charAt(2) + colorChar + group.charAt(3) + colorChar + group.charAt(4) + colorChar + group.charAt(5));
+        }
+
+        return matcher.appendTail(buffer).toString();
+    }
+
+    private Component formatItemInHand(Player player) {
+        ItemStack itemInMainHand = player.getEquipment().getItemInMainHand();
+
+        if (!itemInMainHand.hasItemMeta() || !itemInMainHand.getItemMeta().hasDisplayName()) {
+            return Component.text("[" + WordUtils.capitalizeFully(itemInMainHand.getType().name().toLowerCase().replace("_", " ")) + (itemInMainHand.getAmount() > 1 ? " x" + itemInMainHand.getAmount() : "") + "]").hoverEvent(itemInMainHand.asHoverEvent());
+        }
+
+        return Component.text("[").append(itemInMainHand.getItemMeta().displayName()).append(Component.text((itemInMainHand.getAmount() > 1 ? (" x" + itemInMainHand.getAmount()) : "") + "]")).hoverEvent(itemInMainHand.asHoverEvent());
+    }
+
+    private Component formatInventory(Player player) {
+        final ItemStack[] contents = player.getInventory().getStorageContents();
+        final UUID holder = player.getUniqueId();
+
+        cachedInventories.put(player.getName().toLowerCase(), new CachedInventory(holder, contents.clone()));
+
+        return Component.text("[" + player.getName() + "'s Inventory (/inv " + player.getName() + ")]").hoverEvent(Component.text("Click to view " + player.getName() + "'s inventory.").color(NamedTextColor.GRAY)).clickEvent(ClickEvent.clickEvent(ClickEvent.Action.RUN_COMMAND, "/inv " + player.getName()));
+    }
+
+    private final class CachedInventory {
+
+        private final UUID holder;
+        private final ItemStack[] contents;
+
+        public CachedInventory(UUID holder, ItemStack[] contents) {
+            this.holder = holder;
+            this.contents = contents;
+        }
+
+        public UUID getHolder() {
+            return holder;
+        }
+
+        public ItemStack[] getContents() {
+            return contents;
+        }
+    }
+
+    private final class InventorySnapshot implements InventoryHolder {
+
+        private final Inventory inventory = Bukkit.createInventory(this, 36, "Inventory Snapshot");
+
+        @Override
+        public @NotNull Inventory getInventory() {
+            return inventory;
+        }
+    }
 }
